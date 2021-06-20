152cbfbbbb2a575c99b56600fff6a69e246d2eb7
Niklas Forsström <niklas.forsstrom@mittmedia.se>
2021-06-20 19:51:10 +0200

    Day 14 Two stars

--- day14.js
+++ day14.js
@@ -29,17 +29,42 @@ const update = ({ memoryAdress, value }) => {
   memory[memoryAdress] = decimal;
 }
 
+const floating = (text, parsed = '') => {
+  const [char, ...unparsed] = text;
+  if (char === undefined) return parsed;
+  if (char === '1' || char === '0') return floating(unparsed, parsed + char);
+  if (char === 'X') {
+    return floating(unparsed, parsed + '0') + '@' + floating(unparsed, parsed + '1');
+  }
+}
+
+console.log(floating('00X11X00').split('@'))
+
+const updateTwoStar = ({ memoryAdress, value }) => {
+  const binary = Number(memoryAdress).toString(2).padStart(36, '0');
+  const resultWithFloating = zip(binary, mask)
+    .map(([b, m]) => {
+      if(m === '0') return b;
+      if(m === '1') return '1';
+      if(m === 'X') return 'X';
+  }).join('');
+  floating(resultWithFloating)
+    .split('@')
+    .forEach(address => {
+      memory[Number('0b' + address)] = value;
+  });
+}
+
+
 for(const instruction of instructions) {
   if (instruction.startsWith('mask')) {
     mask = instruction.slice(7); // remove 'mask = '
   }
   if (instruction.startsWith('mem')) {
     const [memoryAdress, value] = instruction.match(/\d+/g).map(Number);
-    update({ memoryAdress, value })
+    updateTwoStar({ memoryAdress, value })
   }
 }
-
-console.log('memory', memory);
 
 const sum = (acc, curr) => acc + curr;
 console.log('sum', Object.values(memory).reduce(sum, 0));
